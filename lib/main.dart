d96d758f6bcb10f85091edfa3358fdfb31779646
dhruva <dhruva.s@surya-digital.com>
2023-03-01 07:58:32 +0530

    Add scrollable list and circular image
    Add image asset
    Scrollable list and button for Home Page.
    Circular image for Profile Page.

--- lib/main.dart
+++ lib/main.dart
@@ -23,14 +23,121 @@ class _MyAppState extends State<MyApp> {
   }
 
   List<Widget> pages = <Widget>[
-    const Text('Home Page',
-        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 10,
+            child: ListView(
+              padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: colors.primaryColor(100),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.all(5),
+                  height: 100,
+                  child: const Center(
+                    child: Text("Entry 1"),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: colors.primaryColor(200),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.all(5),
+                  height: 100,
+                  child: const Center(
+                    child: Text("Entry 2"),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: colors.primaryColor(300),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.all(5),
+                  height: 100,
+                  child: const Center(
+                    child: Text("Entry 3"),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: colors.primaryColor(400),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.all(5),
+                  height: 100,
+                  child: const Center(
+                    child: Text("Entry 4"),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: colors.primaryColor(500),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.all(5),
+                  height: 100,
+                  child: const Center(
+                    child: Text("Entry 5"),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: colors.primaryColor(600),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.all(5),
+                  height: 100,
+                  child: const Center(
+                    child: Text("Entry 6"),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: colors.primaryColor(700),
+                      borderRadius: BorderRadius.circular(10)),
+                  margin: const EdgeInsets.all(5),
+                  height: 100,
+                  child: const Center(
+                    child: Text("Entry 7"),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(5),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text("Button"),
+              //style: ButtonStyle(shape: ),
+            ),
+          )
+        ]),
     const Text('Search Page',
         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
     const Text('My cart',
         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    const Text('Profile Page',
-        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage('assets/images/abstract.jpg'),
+// image: NetworkImage(
+//     'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80'),
+                fit: BoxFit.cover,
+              ),
+              color: colors.primaryColor(900),
+              //shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(90)),
+        ),
+        const Text('Your account')
+      ],
+    ),
   ];
 
   @override
